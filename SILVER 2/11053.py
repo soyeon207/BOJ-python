550aa23a063042676d5f9db12487423a5de89144
soyeon207 <psy010207@naver.com>
2024-11-21 15:58:24 +0900

    ✨ 11053번 가장 긴 증가하는 부분 수열

---
+++ SILVER 2/11053.py
@@ -0,0 +1,8 @@
+A = int(input())
+sequence = [int(i) for i in input().split()]
+dp = [1 for _ in range(A)]
+for i in range(1, A):
+     for j in range(i):
+            if sequence[j] < sequence[i]:
+                dp[i] = max(dp[j] + 1, dp[i])    
+print(max(dp))
